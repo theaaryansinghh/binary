64f9898b180e7ea84e8f25045b1763cb70f79504
aaryansinghh <aaryansinghh@outlook.com>
2026-04-22 20:30:18 +0530

    0000000

--- code.py
+++ code.py
@@ -4,16 +4,11 @@ def image_to_binary(image_path):
     if not os.path.exists(image_path):
         print("File not found")
         return
-
     with open(image_path, "rb") as img:
         data = img.read()
-
     print(f"Size: {len(data)} bytes\n")
-
     for i in range(0, len(data), 4):
         chunk = data[i:i + 4]
         print(' '.join(format(byte, '08b') for byte in chunk))
-
-
 path = input("Enter image path: ").strip()
 image_to_binary(path)
